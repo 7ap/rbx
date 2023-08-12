602ff6cec4af6c986c437762290a9897bd89c438
7ap <109433659+7ap@users.noreply.github.com>
2023-08-12 00:03:24 -0500

    refactor: move tests into mod.rs

--- src/stl/mod.rs
+++ src/stl/mod.rs
@@ -1,3 +1,25 @@
 mod string;
 
 pub use string::String;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn string() {
+        let _ = env_logger::builder().is_test(true).try_init();
+
+        let mut string = String::new();
+        info!("empty: {:?} ({:?})", string.data(), string);
+        assert_eq!("", string.data());
+
+        string.assign("Hello, world!");
+        info!("small: {:?} ({:?})", string.data(), string);
+        assert_eq!("Hello, world!", string.data());
+
+        string.assign("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+        info!("large: {:?} ({:?})", string.data(), string);
+        assert_eq!("ABCDEFGHIJKLMNOPQRSTUVWXYZ", string.data());
+    }
+}
--- src/stl/string.rs
+++ src/stl/string.rs
@@ -88,25 +88,3 @@ impl From<&str> for String {
         string
     }
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn string() {
-        let _ = env_logger::builder().is_test(true).try_init();
-
-        let mut string = String::new();
-        info!("empty: {:?} ({:?})", string.data(), string);
-        assert_eq!("", string.data());
-
-        string.assign("Hello, world!");
-        info!("small: {:?} ({:?})", string.data(), string);
-        assert_eq!("Hello, world!", string.data());
-
-        string.assign("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-        info!("large: {:?} ({:?})", string.data(), string);
-        assert_eq!("ABCDEFGHIJKLMNOPQRSTUVWXYZ", string.data());
-    }
-}
