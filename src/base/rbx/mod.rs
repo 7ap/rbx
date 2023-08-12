30b916a4ca6b79128859cf26fdfdf9677560002a
7ap <109433659+7ap@users.noreply.github.com>
2023-08-12 13:06:41 -0500

    feat: add taskschedulerjob
    might be wrong

--- src/base/rbx/mod.rs
+++ src/base/rbx/mod.rs
@@ -1,11 +1,15 @@
 mod task_queue;
 mod task_scheduler;
+mod task_scheduler_arbiter;
 mod task_scheduler_job;
 mod time;
 
 pub use task_queue::{AsyncTaskQueue, ParallelJobQueue, TaskQueue};
 pub use task_scheduler::TaskScheduler;
-pub use task_scheduler_job::{SharedJobPointerOrder, TaskSchedulerJob};
+pub use task_scheduler_arbiter::TaskSchedulerArbiter;
+pub use task_scheduler_job::{
+    JobPriority, SharedJobPointerOrder, TaskSchedulerJob, TaskSchedulerJobState,
+};
 pub use time::{
     RunningAverage, RunningAverageDutyCycle, RunningAverageTimeInterval, Time, TimeInterval, Timer,
 };
--- src/base/rbx/task_scheduler_job.rs
+++ src/base/rbx/task_scheduler_job.rs
@@ -1,6 +1,56 @@
+use crate::stl::condition_variable::ConditionVariable;
+use crate::stl::memory::SharedPtr;
+use crate::stl::string::String as StlString;
+
+use super::{RunningAverageDutyCycle, TaskSchedulerArbiter, Time, TimeInterval};
+
 #[derive(Debug)]
 #[repr(C)]
-pub struct TaskSchedulerJob {}
+pub enum JobPriority {
+    EarlyRendering,
+    NetworkReceiveIncoming,
+    NetworkPostReplicatorCreation,
+    NetworkProcessIncoming,
+    NetworkProcessPhysicsReceive,
+    NetworkProcessJoin,
+    Default,
+    Simulation,
+    Heartbeat,
+    NetworkStreamingSolver,
+    NetworkMegaOutgoing,
+    NetworkProcessOutgoing,
+    NetworkSendOutgoing,
+    Render,
+    NetworkDisconnectCleanUp,
+    NotOrderDependent,
+}
+
+#[derive(Debug)]
+#[repr(C)]
+pub enum TaskSchedulerJobState {
+    Unknown,
+    Running,
+}
+
+#[derive(Debug)]
+#[repr(C)]
+pub struct TaskSchedulerJob {
+    pub vmt: usize,
+    pub this: SharedPtr<Self>,
+    pub name: StlString,
+    pub arbiter: SharedPtr<TaskSchedulerArbiter>,
+    pub priority: JobPriority,
+    pub step_start_time: Time,
+    pub step_stop_time: Time,
+    pub state: TaskSchedulerJobState,
+    pub time_of_last_step: Time,
+    pub timespan_of_last_step: TimeInterval,
+    pub duty_cycle: RunningAverageDutyCycle,
+    pub job_workers: i32,
+    pub should_notify_completion: bool,
+    _pad0: [u8; 0x003],
+    pub completion_cond: ConditionVariable,
+}
 
 #[derive(Debug)]
 #[repr(C)]
